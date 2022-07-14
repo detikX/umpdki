cc68ec341e18686f8a6f8769c1abfed0b5cbdfaf
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-07-14 15:42:35 +0700

    new update

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 // alert(1);
 
-$(window).scroll(function () {
-  //zoom
-  var top = $(window).scrollTop(),
-    scale = top / 2000;
-  $(".inner").css({
-    transform: "scale(" + scale + "," + scale + ")",
-    "-webkit-transform": "scale(" + scale + "," + scale + ")",
-  });
+// $(window).scroll(function () {
+//   //zoom
+//   var top = $(window).scrollTop(),
+//     scale = top / 2000;
+//   $(".inner").css({
+//     transform: "scale(" + scale + "," + scale + ")",
+//     "-webkit-transform": "scale(" + scale + "," + scale + ")",
+//   });
 
-  var scroll = $(window).scrollTop();
-  // console.log(scroll);
-  var header = $("header");
-  return scroll > 20 ? header.addClass("stuck") : header.removeClass("stuck");
-  // }
-});
+//   var scroll = $(window).scrollTop();
+//   // console.log(scroll);
+//   var header = $("header");
+//   return scroll > 20 ? header.addClass("stuck") : header.removeClass("stuck");
+//   // }
+// });
 
 $(() => {
-  $(window).on("load", function () {
-    $(".preloader").fadeOut(10000);
-    $(".preloader").remove();
-  });
+  // $(window).on("load", function () {
+  //   $(".preloader").fadeOut(10000);
+  //   $(".preloader").remove();
+  // });
   new WOW().init();
   var mobile = $(".menu").addClass("mob");
   $(document).on("click", ".m-menu", () => {
@@ -95,39 +95,80 @@ $(() => {
   });
 });*/
 //slick
+$(window)
+  .scroll(function () {
+    let o = $(window),
+      i = $("body"),
+      t = $(".section");
+    s = o.scrollTop() + o.height() / 3;
+    console.log("o.scrollTop()", o.scrollTop());
+    console.log("o.height", o.height());
+    t.each(function () {
+      console.log("top", $(this).position().top <= s);
+      // console.log("s", s);
+      $(this).position().top <= s &&
+        $(this).position().top + $(this).height() > s &&
+        i.removeClass(
+          $(".section").removeClass("active"),
+          $(this).addClass("active")
+        );
+    });
+  })
+  .scroll();
 
-$(".autoplay").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  arrows: true,
-  autoplaySpeed: 2000,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ],
-});
+if (screen.width > 768) {
+  var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+  function moveBackground() {
+    x += (lFollowX - x) * friction;
+    y += (lFollowY - y) * friction;
+
+    translate = "translate(" + y + "px, " + x + "px) scale(0.9)";
+    translatea = "translate(" + y + "px, " + x + "px) scale(0.9)";
+
+    $(".cover__cloud").css({
+      "-webit-transform": translatea,
+      "-moz-transform": translatea,
+      transform: translatea,
+    });
+
+    window.requestAnimationFrame(moveBackground);
+  }
+
+  $(window).on("mousemove click", function (e) {
+    var lMouseX = Math.max(
+      -100,
+      Math.min(100, $(window).width() / 2 - e.clientX)
+    );
+    var lMouseY = Math.max(
+      -100,
+      Math.min(100, $(window).height() / 2 - e.clientY)
+    );
+    lFollowX = (5 * lMouseY) / 100; // 100 : 12 = lMouxeX : lFollow
+    lFollowY = (40 * lMouseX) / 100;
+  });
+
+  moveBackground();
+
+  // sr.reveal(".align-left .credit__team", {
+  //   delay: 300,
+  //   origin: "right",
+  // });
+  // sr.reveal(".align-left .credit__member", {
+  //   delay: 450,
+  //   origin: "right",
+  // });
+
+  // sr.reveal(".align-right .credit__team", {
+  //   delay: 300,
+  //   origin: "left",
+  // });
+  // sr.reveal(".align-right .credit__member", {
+  //   delay: 450,
+  //   origin: "left",
+  // });
+}
---
+++ js/chart.js
@@ -0,0 +1,100 @@
+const labels = ["2017", "2018", "2019", "2020", "2021", "2022"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Upah Minimum Pendapatan",
+      backgroundColor: "#28B7FC",
+      borderColor: "royalblue",
+      data: [3355750, 3648035, 3940973, 4276349, 4416186, 4641854],
+    },
+  ],
+};
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "UMP Era Anies Baswedan",
+        color: "black",
+        font: {
+          family: "'Montserrat'", // Your font family
+          size: 20,
+          weight: "bold",
+        },
+
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+      },
+      legend: {
+        position: "bottom",
+        labels: {
+          font: {
+            family: "'Montserrat'",
+          },
+        },
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          color: "black",
+          display: true,
+          text: "Tahun",
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 14,
+            weight: "bold",
+          },
+        },
+        ticks: {
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 14,
+          },
+        },
+      },
+      y: {
+        title: {
+          color: "black",
+          display: true,
+          text: "Gaji",
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 14,
+            weight: "bold",
+          },
+          padding: 20,
+        },
+        ticks: {
+          font: {
+            family: "'Montserrat'", // Your font family
+            size: 14,
+          },
+        },
+      },
+    },
+    animations: {
+      y: {
+        easing: "easeInOutElastic",
+        from: (ctx) => {
+          if (ctx.type === "data") {
+            if (ctx.mode === "default" && !ctx.dropped) {
+              ctx.dropped = true;
+              return 0;
+            }
+          }
+        },
+      },
+    },
+  },
+};
+const myChart = new Chart(document.getElementById("myChart"), config);
